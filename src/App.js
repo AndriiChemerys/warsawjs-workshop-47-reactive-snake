d0978e3d7b0a385d5ddc8299177cc80d76cf97af
AndriiChemerys <andriichemerys@gmail.com>
2020-08-23 13:56:08 +0200

    Adding guideline

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import GameGrid from "./game/GameGrid";
 
 const GridSize = 10;
@@ -18,9 +18,52 @@ function App() {
     y: randomIndex(GridSize),
   });
 
-  const direction = "up";
-  const speed = 500;
+  const direction = useRef("up");
+  const [speed, setSpeed] = useState(500);
 
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const [snakeHead] = snake;
+      const newSnakeHead = { ...snakeHead };
+
+      if (direction === "up") {
+        newSnakeHead.x -= 1;
+      } else if (direction === "down") {
+        newSnakeHead.x += 1;
+      } else if (direction === "right") {
+        newSnakeHead.y += 1;
+      } else if (direction === "left") {
+        newSnakeHead.y -= 1;
+      }
+
+      const newSnake = [newSnakeHead, ...snake.slice(0, snake.length - 1)];
+      setSnake(newSnake);
+    }, speed);
+    return () => {
+      clearInterval(interval);
+    };
+  }, [snake]);
+
+  function handleKeyDown(event) {
+    if (event.key === "ArrowUp") {
+      direction.current = "up";
+    } else if (event.key === "ArrowDown") {
+      direction.current = "down";
+    } else if (event.key === "ArrowLeft") {
+      direction.current = "left";
+    } else if (event.key === "ArrowRight") {
+      direction.current = "right";
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+  // return direction.current;
   return <GameGrid gridSize={GridSize} fruit={fruit} snake={snake} />;
 }
 
