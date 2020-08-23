dcee745086f82cce684c401041dafa8fb8b156a2
AndriiChemerys <andriichemerys@gmail.com>
2020-08-23 12:48:01 +0200

    Placing snake and dot on the field

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import GameGrid from "./game/GameGrid";
 
 const GridSize = 10;
 
+function randomIndex(n) {
+  return Math.floor(Math.random() * n);
+}
+
 function App() {
-  return <GameGrid gridSize={GridSize} />;
+  const [snake, setSnake] = useState([
+    { x: GridSize / 2, y: GridSize / 2 },
+    { x: GridSize / 2, y: GridSize / 2 + 1 },
+    { x: GridSize / 2, y: GridSize / 2 + 2 },
+  ]);
+  const [fruit, setFruit] = useState({
+    x: randomIndex(GridSize),
+    y: randomIndex(GridSize),
+  });
+
+  const direction = "up";
+  const speed = 500;
+
+  return <GameGrid gridSize={GridSize} fruit={fruit} snake={snake} />;
 }
 
 export default App;
--- src/game/GameGrid.jsx
+++ src/game/GameGrid.jsx
@@ -1,17 +1,28 @@
 import React from "react";
 import "./GameGrid.css";
 
-export default function GameGrid({ gridSize }) {
+export default function GameGrid({ gridSize, fruit, snake }) {
   const indexes = Array.from({ length: gridSize }).map((_, index) => index);
   console.log(["GameGrid"], { gridSize, indexes });
+
+  function getCellClass(x, y) {
+    if (snake.some((snakePart) => snakePart.x === x && snakePart.y === y)) {
+      return "snakeCell";
+    }
+
+    if (x === fruit.x && y === fruit.y) {
+      return "fruitCell";
+    }
+
+    return "gridCell";
+  }
+
   return (
     <div className="grid">
       {indexes.map((x) => (
         <div className="gridRow" key={x}>
           {indexes.map((y) => (
-            <div className="gridCell" key={`${x}x${y}`}>
-              {`${x}x${y}`}
-            </div>
+            <div className={getCellClass(x, y)} key={`${x}x${y}`} />
           ))}
         </div>
       ))}
